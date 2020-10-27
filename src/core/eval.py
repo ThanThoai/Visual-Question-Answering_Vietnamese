77d48b93e59f5af82283631f82e3a534d912e436
ThanThoai <Darkrai17071998@gmail.com>
2020-10-28 01:30:20 +0700

    upload anything

--- src/core/eval.py
+++ src/core/eval.py
@@ -3,6 +3,8 @@ from tqdm import tqdm
 import os
 import glob
 import json 
+import numpy as np
+import pickle 
 
 class Eval:
     
@@ -210,3 +212,54 @@ class Eval:
             
         avg_score = float(sum_score) / sum_overall
         return avg_score
+    
+    
+
+def eval(__C, dataset, ans_ix_list, pred_list, result_eval_file, ensemble_file, log_file, valid = False):
+    
+    result_eval_file = result_eval_file + '.json'
+    qid_list = [qid for qid in dataset.qid_list]
+    ans_size = dataset.ans_size
+    
+    result = [{
+        "questionId" : qid_list[ix],
+        "prediction" : dataset.ix_to_ans[str(ans_ix_list[ix])],
+    } for ix in range(len(qid_list))]
+    
+    print("[INFO] Save the result to file: {}".format(result_eval_file))
+    json.dump(result, open(result_eval_file, 'w'))
+    
+    if __C.TEST_SAVE_PRED:
+        print("[INFO] Save the prediction vector to file: {}".format(ensemble_file)) 
+        
+        pred_list = np.array(pred_list).reshape(-1, ans_size)
+        result_pred = [{
+            'pred' : pred_list[ix],
+            'qid' : int(qid_list[ix])
+        } for ix in range(qid_list.__len__())]
+        
+        pickle.dump(result_pred, open(ensemble_file, 'wb+'), protocol = -1)
+        
+    if valid:
+        ques_file_path = __C.RAW_PATH[__C.DATASET][__C.SPLIT['val']]
+        choice_path = None
+        
+        if __C.SPLIT['val'] + '_choices' in __C.RAW_PATH[__C.DATASET]:
+            choice_path = __C.RAW_PATH[__C.DATASET][__C.SPLIT['val'] + '_choices']
+            
+        eval_gqa = Eval(__C, result_eval_file, ques_file_path, choice_path, EVAL_CONSISTENCY = False)
+        result_string, detail_result_string = eval_gqa.get_str_result()
+        
+        print("[INFO] Write to log file: {}".format(log_file))
+        log = open(log_file, 'a+')
+        for result_str in result_string:
+            log.write(result_str + '\n')
+            print(result_str)
+            
+        for detail_str in detail_result_string:
+            log.write(detail_str + '\n')
+        
+        log.write('\n')
+        log.close()
+ 
+        
