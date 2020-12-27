e775c5b4663ddcc36ee4641b8feee2b266c44cf6
Bùi Xuân Thoại (VNCDLL-CTÐTKSAI) <thoaibx@vingroup.net>
2020-12-27 23:54:32 +0700

    edit merge_raw_data.py

--- merge_raw_data.py
+++ merge_raw_data.py
@@ -94,13 +94,14 @@ def main(path_en, path_vi, path_idx, path_json, type_ = "train"):
     print(list(dict_question.keys())[:10])
     for question in data_json["questions"]:
         image_id = str(question["image_id"])
-        qid = question["question_id"]
+        qid = str(question["question_id"])
         q = question["question"]
         for key, value in dict_question[image_id].items():
             if check_string(q, value):
                 new_idx[int(key)] = qid
                 count_check += 1
                 break
+    print(count_check)
     with open(f"new_vi_{type_}.txt", "w") as wr:
         for question in data_vi[:-1]:
             wr.write(question)
